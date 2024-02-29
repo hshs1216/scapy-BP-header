ed024ad36ef86bf3f7d4a368a773387c7650a799
hshs1216 <155788335+hshs1216@users.noreply.github.com>
2024-02-29 11:36:08 +0900

    Update udp_send_receive.c

--- udp_send_receive.c
+++ udp_send_receive.c
@@ -9,22 +9,25 @@
 #include <pthread.h>
 
 #define BUF_SIZE 1024
-#define DEST_PORT 12345      // 送信先のポート番号
+#define DEST_PORT 12345
+
+typedef struct {
+    int sockfd;
+    char dest_ip[INET_ADDRSTRLEN];
+} thread_args_t;
 
 void *send_function(void *arg) {
-    int sockfd = *((int *)arg);
+    thread_args_t *args = (thread_args_t *)arg;
     struct sockaddr_in destaddr;
     char message[] = "Hello from sender";
 
-    // 送信先のアドレス情報の設定
     memset(&destaddr, 0, sizeof(destaddr));
     destaddr.sin_family = AF_INET;
     destaddr.sin_port = htons(DEST_PORT);
-    inet_pton(AF_INET, arg, &destaddr.sin_addr);
+    inet_pton(AF_INET, args->dest_ip, &destaddr.sin_addr);
 
-    // メッセージの送信
     while (1) {
-        if (sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&destaddr, sizeof(destaddr)) < 0) {
+        if (sendto(args->sockfd, message, strlen(message), 0, (struct sockaddr *)&destaddr, sizeof(destaddr)) < 0) {
             perror("sendto failed");
             exit(EXIT_FAILURE);
         }
@@ -47,33 +50,32 @@ int main(int argc, char *argv[]) {
     socklen_t addrlen;
     ssize_t recvlen;
     pthread_t send_thread;
+    thread_args_t args;
 
-    // ソケットの作成
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // サーバーのアドレス情報の設定
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(12345);
 
-    // ソケットにアドレスをバインド
     if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // 送信スレッドの作成
-    if (pthread_create(&send_thread, NULL, send_function, argv[1]) != 0) {
+    args.sockfd = sockfd;
+    strncpy(args.dest_ip, argv[1], INET_ADDRSTRLEN);
+
+    if (pthread_create(&send_thread, NULL, send_function, &args) != 0) {
         perror("pthread_create failed");
         exit(EXIT_FAILURE);
     }
 
-    // データの受信
     while (1) {
         addrlen = sizeof(cliaddr);
         recvlen = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&cliaddr, &addrlen);
@@ -82,13 +84,12 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        // 受信したデータの表示
         buffer[recvlen] = '\0';
         printf("Received: %s\n", buffer);
     }
 
-    // ソケットのクローズ
     close(sockfd);
 
     return 0;
 }
+``
